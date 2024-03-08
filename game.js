6dd7925b38c4cab85db720bcc3fce82f7107bb51
MiltonCoronado <miltonext@hotmail.com>
2024-03-07 21:05:19 -0500

    game modified

--- game.js
+++ game.js
@@ -8,6 +8,11 @@ const context = canvas.getContext('2d');
 let canvasSise;
 let elementSize;
 
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
 window.addEventListener('load', setCanvasSize)//con "Window.addEvenlistener" hacemos que el html espere un evento, en este caso que cargue todo el HTML para que se ejecute la Fn "resize" y esta Fn ejecuta "startGame". es lo mas recomendable usando un CANVAS!!!
 window.addEventListener('resize', setCanvasSize)
 
@@ -34,20 +39,34 @@ function startGame() {
   context.font = elementSize + 'px Verdana';
   context.textAlign = 'start'
 
-  const map = maps[2]
+  const map = maps[1]
   const mapRow = map.trim().split('\n');//quita los espacios en blanco del array, luego crea arrays separandolos por saltode linea.
   const mapRowCols = mapRow.map(item => item.trim().split(''))//quita los espacios en blanco de cada array de string, luego separa los string del array en elemento por elemento.
   console.log(mapRowCols)
   
+  context.clearRect(0 , 0, canvasSise, canvasSise)
   mapRowCols.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
       const emoji = emojis[col];
       const postX = elementSize * colIndex;
       const postY = elementSize * (rowIndex + 1);
+
+      if(col === 'O') {
+        if(!playerPosition.x && !playerPosition.y){
+          playerPosition.x = postX;
+          playerPosition.y = postY;
+        }
+      }
+      
       context.fillText(emoji, postX , postY)
-      console.log({row, rowIndex, col, colIndex})
     })
   });
+
+  movePlayer();
+};
+
+function movePlayer() {
+  context.fillText([emojis['PLAYER']], playerPosition.x, playerPosition.y)
 };
 
 btnUp.addEventListener('click', moveUp);
@@ -83,18 +102,44 @@ function moveByKeys(event) {
 
 function moveUp() {
   console.log('Me quiero mover hacia arriba')
+
+  if((playerPosition.y - elementSize) < elementSize){
+    console.log('OUT')
+  }else {
+    playerPosition.y -= elementSize
+    startGame();
+  }
 };
 
 function moveLeft() {
   console.log('Me quiero mover a la izquierda')
+  if((playerPosition.x - elementSize) < 0){
+    console.log('OUT')
+  }else {
+    playerPosition.x -= elementSize
+    startGame();
+  }
 };
 
 function moveRight() {
   console.log('Me quiero mover a la derecha')
+  if((playerPosition.x + elementSize) > (canvasSise - elementSize)){
+    console.log('OUT')
+  }else {
+    playerPosition.x += elementSize
+    startGame();
+  }
 };
 
 function moveDown() {
   console.log('Me quiero mover hacia abajo')
+  console.log('Me quiero mover a la derecha')
+  if((playerPosition.y + elementSize) > canvasSise){
+    console.log('OUT')
+  }else {
+    playerPosition.y += elementSize
+    startGame();
+  }
 };
 
 
